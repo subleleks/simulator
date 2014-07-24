500624b7e9201eb67d3107d15fdf550c3767035f
Pimenta <matheuscscp@gmail.com>
2014-07-23 22:37:46 -0300

    Now we have two implementations. The old with 64 bit words and the new with 32 bit words

--- main.cpp
+++ main.cpp
@@ -5,75 +5,8 @@
  *      Author: Pimenta
  */
 
-#include <cmath>
-#include <cstdint>
-#include <cstdio>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-typedef int64_t  word_t;
-typedef uint64_t uword_t;
-typedef uint32_t address_t;
-
-#ifndef MEM_WORDS
-#define MEM_WORDS 0x2000
-#endif
-
-const uword_t ADDRESS_WIDTH = uword_t(log2(double(MEM_WORDS)));
-const uword_t ADDRESS_MASK  = uword_t(MEM_WORDS - 1);
-
+int simulator32(int argc, char* argv[]);
+int simulator64(int argc, char* argv[]);
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage mode: subleq-sim <meminit_file>\n");
-    return 0;
-  }
-  
-  uword_t* mem = new uword_t[MEM_WORDS];
-  
-  // read data
-  ifstream f(argv[1]);
-  for (address_t words = 0; ; ++words) {
-    string tmp;
-    
-    // skip address
-    f >> tmp;
-    if (tmp == "END;") {
-      break;
-    }
-    while (f.get() != ':');
-    f.get();
-    
-    // read word
-    tmp = "";
-    for (char c = f.get(); c != ';'; c = f.get())
-      tmp += c;
-    sscanf(tmp.c_str(), "%llx", &mem[words]);
-  }
-  f.close();
-  
-  // run
-  uword_t instruction;
-  address_t Aaddr, Baddr, Jaddr;
-  word_t sub;
-  uword_t instructions = 0;
-  for (address_t ip = 0; ; ip = sub <= 0 ? Jaddr : ip + 1) {
-    instruction = mem[ip];
-    if (0 > (word_t)instruction) {
-      break;
-    }
-    Aaddr = (instruction >> 2*ADDRESS_WIDTH) & ADDRESS_MASK;
-    Baddr = (instruction >> 1*ADDRESS_WIDTH) & ADDRESS_MASK;
-    Jaddr = (instruction >> 0*ADDRESS_WIDTH) & ADDRESS_MASK;
-    sub = mem[Baddr] - mem[Aaddr];
-    printf("0x%08X: A=0x%08X(0x%016llX) B=0x%08X(0x%016llX) J=0x%08X sub=0x%016llX\n", ip, Aaddr, mem[Aaddr], Baddr, mem[Baddr], Jaddr, sub);
-    mem[Baddr] = sub;
-    instructions++;
-  }
-  printf("Instructions executed: %lld\n", instructions);
-  
-  delete[] mem;
-  
-  return 0;
+  return simulator32(argc, argv);
 }
---
+++ simulator32.cpp
@@ -0,0 +1,73 @@
+/*
+ * simulator32.cpp
+ *
+ *  Created on: Jul 23, 2014
+ *      Author: Pimenta
+ */
+
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+typedef int32_t  word_t;
+typedef uint32_t uword_t;
+
+#ifndef MEM_WORDS
+#define MEM_WORDS 0x2000
+#endif
+
+int simulator32(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage mode: subleq-sim <meminit_file>\n");
+    return 0;
+  }
+  
+  uword_t* mem = new uword_t[MEM_WORDS];
+  
+  // read data
+  ifstream f(argv[1]);
+  for (uword_t words = 0; ; words++) {
+    string tmp;
+    
+    // skip address
+    f >> tmp;
+    if (tmp == "END;") {
+      break;
+    }
+    while (f.get() != ':');
+    f.get();
+    
+    // read word
+    tmp = "";
+    for (char c = f.get(); c != ';'; c = f.get())
+      tmp += c;
+    sscanf(tmp.c_str(), "%x", &mem[words]);
+  }
+  f.close();
+  
+  // run
+  uword_t Aaddr, A, B, J;
+  word_t sub;
+  uint64_t instructions = 0;
+  for (uword_t ip = 0; ; ip = sub <= 0 ? J : ip) {
+    if (mem[ip] == mem[ip + 1] && mem[ip + 2] == ip) {
+      break;
+    }
+    
+    A = mem[ip++];
+    B = mem[ip++];
+    J = mem[ip++];
+    sub = mem[B] - mem[A];
+    printf("0x%08X: A=0x%08X(0x%08X) B=0x%08X(0x%08X) J=0x%08X sub=0x%08X\n", ip, A, mem[A], B, mem[B], J, sub);
+    mem[B] = sub;
+    instructions++;
+  }
+  printf("Instructions executed: %lld\n", instructions);
+  
+  delete[] mem;
+  
+  return 0;
+}
